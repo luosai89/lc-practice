9ac84c20db59b30fef650b831c6d9ab8a5edcc27
Sai Luo <luosai@amazon.com>
2021-04-01 19:49:18 -0700

    [20210401][E766]ToeplitzMatrix

---
+++ E766ToeplitzMatrix.java
@@ -0,0 +1,17 @@
+class E766ToeplitzMatrix {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        for (int cr = 0; cr < r - 1; cr++) {
+            for (int cc = 0; cc < c - 1; cc++) {
+                int val = matrix[cr][cc];
+                int t = 1;
+                while (cr + t < r && cc + t < c) {
+                    if (matrix[cr+t][cc+t] != val) return false;
+                    t++;
+                }
+            }
+        }
+        return true;
+    }
+}
