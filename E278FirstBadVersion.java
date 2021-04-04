d8a45aaf35506d3e03ea766fcff98e742b3476e3
Sai Luo <luosai@amazon.com>
2021-04-04 13:21:23 -0700

    4/4/2021 Easy 278 First Bad Version

---
+++ E278FirstBadVersion.java
@@ -0,0 +1,29 @@
+/**
+ * 4/4/2021 https://leetcode.com/problems/first-bad-version/
+ * Runtime and memory usage seemed lower than ideal. Need to check up-voted answers.
+ */
+public class E278FirstBadVersion {
+    public static int firstBadVersion(int n) {
+        int lastGood = 0;
+        int start = 1;
+        int end = n;
+        while (start < end) {
+            // had to consider the largest int value here! mid turned negative!
+            int mid = start/2 + end/2;
+            if (isBadVersion(mid)) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+                lastGood = mid;
+            }
+        }
+        return lastGood + 1;
+    }
+    private static boolean isBadVersion(int n) {
+        return n >= 1702766719;
+    }
+    public static void main(String[] args) {
+        int result = firstBadVersion(2126753390);
+        System.out.println(result);
+    }
+}
