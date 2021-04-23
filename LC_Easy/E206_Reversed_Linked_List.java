c4568642476b7d4915a9d97a108d962b24f002bf
Sai Luo <luosai@amazon.com>
2021-04-22 21:53:32 -0700

    4/16/2021 Easy Reversed Linked List

--- LC_Easy/E206_Reversed_Linked_List.java
+++ LC_Easy/E206_Reversed_Linked_List.java
@@ -1,5 +1,26 @@
 package LC_Easy;
 
+/**
+ * 4/16/2021 https://leetcode.com/problems/reverse-linked-list/
+ */
 public class E206_Reversed_Linked_List {
-    
+
+    public ListNode reverseListRecursive(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode newHead = reverseListRecursive(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public ListNode reverseListIterative(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode nextHolder = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = nextHolder;
+        }
+        return newHead;
+    }
 }
