de1bb2fc36df31d6c174fb8427082575bf5501e2
Sai Luo <luosai@amazon.com>
2021-05-11 19:15:06 -0700

    Easy 237 Delete Node in a Linked List

---
+++ LC_Easy/E_0237_Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,13 @@
+package LC_Easy;
+
+/**
+ * 5/11/2021 https://leetcode.com/problems/delete-node-in-a-linked-list/
+ */
+
+public class E_0237_Delete_Node_in_a_Linked_List {
+    public void deleteNode(ListNode node) {
+        // because not tail node, node.next is non-null
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
