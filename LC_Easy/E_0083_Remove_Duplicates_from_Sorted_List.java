2457390587f5b5f7f4a5c3ba210f6ef868927688
Sai Luo <luosai@amazon.com>
2021-05-11 19:26:22 -0700

    Easy 83 Remove Duplicates from Sorted List

---
+++ LC_Easy/E_0083_Remove_Duplicates_from_Sorted_List.java
@@ -0,0 +1,19 @@
+package LC_Easy;
+
+/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ */
+
+public class E_0083_Remove_Duplicates_from_Sorted_List {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode t = head;
+        while (t != null && t.next != null) {
+            if (t.next.val == t.val) {
+                t.next = t.next.next;
+            } else {
+                t = t.next;
+            }
+        }
+        return head;
+    }
+}
