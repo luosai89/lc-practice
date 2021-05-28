f895ba4e147806f2f98d71be4a34355ccad5546f
Sai Luo <luosai@amazon.com>
2021-05-27 19:44:24 -0700

    5/27/2021 Tipp LBC1 Counting Pairs

---
+++ TIPP/LBC_W1_Counting_Pairs.java
@@ -0,0 +1,19 @@
+package TIPP;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LBC_W1_Counting_Pairs {
+
+    public static int countPairs(List<Integer> numbers, int k) {
+        int count = 0;
+        Set<Integer> unique = new HashSet<>(numbers);
+        for (Integer n : numbers) {
+            if (unique.contains(n + k)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
