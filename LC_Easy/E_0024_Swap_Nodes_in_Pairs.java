ea40811f932fefb0e285594e01a3dc454d4aa080
Sai Luo <luosai@amazon.com>
2021-06-10 21:49:37 -0700

    6/10/2021 Medium 24 Swap Nodes in Pairs (Linked List)

---
+++ LC_Easy/E_0024_Swap_Nodes_in_Pairs.java
@@ -0,0 +1,22 @@
+package LC_Easy;
+
+/**
+ * https://leetcode.com/problems/swap-nodes-in-pairs/
+ * #1 - 06/10/2021
+ */
+public class E_0024_Swap_Nodes_in_Pairs {
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode newHead = new ListNode();
+        newHead.next = head;
+        ListNode t = newHead;
+        while (t.next != null && t.next.next != null) {
+            ListNode temp = t.next.next;
+            t.next.next = temp.next;
+            temp.next = t.next;
+            t.next = temp;
+            t = t.next.next;
+        }
+        return newHead.next;
+    }
+}
