f3170241398442c3fad0faff0d85de0c9337cf23
Sai Luo <luosai@amazon.com>
2021-08-23 19:39:59 -0700

    8/23 2R M_0560_Subarray_Sum_Equals_K

--- LC_Medium/M_0560_Subarray_Sum_Equals_K.java
+++ LC_Medium/M_0560_Subarray_Sum_Equals_K.java
@@ -3,28 +3,34 @@ package LC_Medium;
 import java.util.HashMap;
 
 /**
- * 6/8/2021 https://leetcode.com/problems/subarray-sum-equals-k/
+ * 2021: 6/8 (R), 8/23 (R)
+ * Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
  * Laioffer explanation https://www.youtube.com/watch?v=aYfwus5T3Bs
  */
 public class M_0560_Subarray_Sum_Equals_K {
 
     // Most optimal - prefix-sum, hashmap
-    public int subarraySum(int[] nums, int k) {
+    public static int subarraySum(int[] nums, int k) {
         if (nums == null || nums.length == 0) return 0;
+        // use prefix sum as key, and frequency (count) as value
+        // we cannot use a set because we are not looking up by index but by target value
         HashMap<Integer, Integer> sums = new HashMap<>();
-        sums.put(0, 1);
+        sums.put(0, 1); // if the current sum is k, then find k - k = 0 and increment count by 1
         int preSum = 0;
         int count = 0;
         for (int n : nums) {
+            // calculate new current sum with new n
             preSum += n;
+            // if k = 5, current sum is 7, yet we have 2 of -2 from previous sums, then there's 2 subarrays
             count += sums.getOrDefault(preSum - k, 0);
+            // put the current sum result
             sums.put(preSum, sums.getOrDefault(preSum, 0) + 1);
         }
         return count;
     }
 
     // Space efficient
-    public int subarraySum2(int[] nums, int k) {
+    public static int subarraySum2(int[] nums, int k) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             int preSum = 0;
@@ -34,5 +40,10 @@ public class M_0560_Subarray_Sum_Equals_K {
             }
         }
         return count;
+    }
+
+    public static void main(String[] args) {
+        int result1 = subarraySum(new int[]{1,1,1}, 2);
+        int result2 = subarraySum(new int[]{1,2,3}, 3);
     }
 }
